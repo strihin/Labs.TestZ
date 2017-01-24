ca5118c8cea7b8b3101ecb46a26de805ae98eb10
Aleksandr Strihin <aistrigin92@gmail.com>
2017-01-24 08:04:55 +0200

    first sprint

---
+++ TestZ/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace TestZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.zedGraph1 = new ZedGraph.ZedGraphControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rb4 = new System.Windows.Forms.RadioButton();
+            this.rb3 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fx = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LaGrange = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Newt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MNK = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblE = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // zedGraph1
+            // 
+            this.zedGraph1.AutoScroll = true;
+            this.zedGraph1.EditModifierKeys = System.Windows.Forms.Keys.D9;
+            this.zedGraph1.Location = new System.Drawing.Point(87, 48);
+            this.zedGraph1.Name = "zedGraph1";
+            this.zedGraph1.ScrollGrace = 0D;
+            this.zedGraph1.ScrollMaxX = 0D;
+            this.zedGraph1.ScrollMaxY = 0D;
+            this.zedGraph1.ScrollMaxY2 = 0D;
+            this.zedGraph1.ScrollMinX = 0D;
+            this.zedGraph1.ScrollMinY = 0D;
+            this.zedGraph1.ScrollMinY2 = 0D;
+            this.zedGraph1.Size = new System.Drawing.Size(520, 350);
+            this.zedGraph1.TabIndex = 0;
+            this.zedGraph1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-1, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 61);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "GO!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20"});
+            this.comboBox1.Location = new System.Drawing.Point(49, 151);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(38, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rb4);
+            this.panel1.Controls.Add(this.rb3);
+            this.panel1.Controls.Add(this.rb2);
+            this.panel1.Controls.Add(this.rb1);
+            this.panel1.Location = new System.Drawing.Point(-1, 48);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(88, 100);
+            this.panel1.TabIndex = 8;
+            // 
+            // rb4
+            // 
+            this.rb4.AutoSize = true;
+            this.rb4.Location = new System.Drawing.Point(4, 76);
+            this.rb4.Name = "rb4";
+            this.rb4.Size = new System.Drawing.Size(58, 17);
+            this.rb4.TabIndex = 3;
+            this.rb4.TabStop = true;
+            this.rb4.Text = "beta=2";
+            this.rb4.UseVisualStyleBackColor = true;
+            // 
+            // rb3
+            // 
+            this.rb3.AutoSize = true;
+            this.rb3.Location = new System.Drawing.Point(4, 52);
+            this.rb3.Name = "rb3";
+            this.rb3.Size = new System.Drawing.Size(67, 17);
+            this.rb3.TabIndex = 2;
+            this.rb3.TabStop = true;
+            this.rb3.Text = "beta=1.5";
+            this.rb3.UseVisualStyleBackColor = true;
+            // 
+            // rb2
+            // 
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(4, 28);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(58, 17);
+            this.rb2.TabIndex = 1;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "beta=1";
+            this.rb2.UseVisualStyleBackColor = true;
+            // 
+            // rb1
+            // 
+            this.rb1.AutoSize = true;
+            this.rb1.Location = new System.Drawing.Point(4, 5);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(67, 17);
+            this.rb1.TabIndex = 0;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "beta=0.5";
+            this.rb1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.X,
+            this.Fx,
+            this.LaGrange,
+            this.Newt,
+            this.MNK});
+            this.dataGridView1.Location = new System.Drawing.Point(607, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(550, 443);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // X
+            // 
+            this.X.FillWeight = 10F;
+            this.X.HeaderText = "X";
+            this.X.Name = "X";
+            this.X.ReadOnly = true;
+            // 
+            // Fx
+            // 
+            this.Fx.HeaderText = "F(x)";
+            this.Fx.Name = "Fx";
+            this.Fx.ReadOnly = true;
+            // 
+            // LaGrange
+            // 
+            this.LaGrange.HeaderText = "LaGrange";
+            this.LaGrange.Name = "LaGrange";
+            this.LaGrange.ReadOnly = true;
+            // 
+            // Newt
+            // 
+            this.Newt.HeaderText = "Newton";
+            this.Newt.Name = "Newt";
+            this.Newt.ReadOnly = true;
+            // 
+            // MNK
+            // 
+            this.MNK.HeaderText = "MNK";
+            this.MNK.Name = "MNK";
+            this.MNK.ReadOnly = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(191, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(293, 51);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Nodes:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 403);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(220, 402);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 16;
+            // 
+            // lblE
+            // 
+            this.lblE.AutoSize = true;
+            this.lblE.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblE.Location = new System.Drawing.Point(0, 243);
+            this.lblE.Name = "lblE";
+            this.lblE.Size = new System.Drawing.Size(0, 13);
+            this.lblE.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1149, 443);
+            this.Controls.Add(this.lblE);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.zedGraph1);
+            this.Name = "Form1";
+            this.Text = "Course work";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraph1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rb4;
+        private System.Windows.Forms.RadioButton rb3;
+        private System.Windows.Forms.RadioButton rb2;
+        public System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fx;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LaGrange;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Newt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MNK;
+        private System.Windows.Forms.Label lblE;
+    }
+}
+
---
+++ TestZ/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace TestZ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();  
+        }        
+        
+        int Beta()
+        {
+            int beta = 0;
+            var rb = new List<RadioButton> {rb1, rb2, rb3, rb4};
+            foreach (RadioButton t in rb)
+            {
+                beta += 2;
+                if (t.Checked)
+                    return beta;
+            }
+            return 2;
+        }
+
+        public double M(double x)
+        {
+            return Math.Pow(16, Math.Pow(Math.Sin(x), Math.Cos(x))) + 3 * Math.Pow(4, Math.Sin(2 * x) / 2) - Beta();
+        }
+
+#region Approximation
+       double MethodInterval(double x, double[] nodes, int k) { return  x * Math.Pow(nodes[k], x); }
+
+       double SKO(double x, double[] nodes, int k, double[] xval)
+       {
+           return  Math.Pow(Math.Pow(M(x) - MethodPoint(x, nodes, k, xval), 2)*Math.Pow(nodes.Count(),-1),0.5);
+       }
+        double MethodPoint(double x, double[] nodes, int k, double[] xval)
+        {
+
+            double mi = Math.Pow(nodes.Count()-1, -1) + nodes[k] * xval[k] / x;
+            return mi;
+        }
+        public double Apr(double x, double[] nodes, int k, double[] xval)
+        {
+           // return m(x)*xval[k]/x + Math.Pow(nodes.Count(), -1);
+             return MethodPoint(x, nodes, k,xval);         
+        }
+        
+
+        double Ee(double[] nodes, double[] xval, int xmax, ref int outcount)
+        {
+            
+            int k = 0;
+            double ma=0;
+            var yStar = new double[3];
+            var y = new double[3];
+            var Em = new double[7];
+            yStar[0] = (xval[0] + xval[xmax-2]) / 2;
+            yStar[1] = Math.Sqrt(Math.Abs(xval[0]) * xval[xmax-2]);
+            yStar[2] = 2 * (xval[0] * xval[xmax-2]) / xval[0] + xval[xmax-2];
+            y[0] = (nodes[0] + nodes[xmax-2]) / 2;
+            y[1] = Math.Sqrt(Math.Abs(nodes[0] * nodes[xmax-2]));
+            y[2] = 2 * (nodes[0] * nodes[xmax-2]) / nodes[0] + nodes[xmax-2];
+
+            for (int i = 0; i < 7; i++)
+            {
+                if(i>=0 && i<=2)
+                    Em[i] = Math.Abs(yStar[0] - y[i]);
+                
+                if (i==3)
+                {
+                    Em[i] = Math.Abs(yStar[1] - y[0]);
+                }
+                if (i > 3 && i<7)
+                {
+
+                    Em[i] = Math.Abs(yStar[2] - y[k]);
+                    k++;
+                }
+                ma = Em.Min();                
+                
+            }
+
+            for (int i = 0; i < 7; i++)
+            {
+                if (ma == Em[i])
+                {
+                    outcount = i;
+                }
+            }
+            return ma;
+        }
+        
+#endregion
+
+       public double PnLagrange(double x, double[] nodes, double[] xval)
+       {
+            double p = 1;
+            double pn = 0;
+            
+           for (int i = 0; i < xval.Count(); i++)
+           {
+               double currentX = xval[i];
+               for (int j = 0; j < xval.Count(); j++)
+               {
+                   if (currentX != xval[j])
+                   {
+                      
+                       p*=(x-xval[j])/(currentX-xval[j]);
+                   }
+               }
+               pn +=nodes[i]*p;
+               p = 1;
+           }
+
+           return pn;
+         }
+       public double Newton(double[] xval, double[] node, double x)
+       {
+           
+           double res = node[0], f, den;
+           int i, j, k;
+           for (i = 1; i < node.Count(); i++)
+           {
+               f = 0;
+               for (j = 0; j <= i; j++)
+               {//следующее слагаемое полинома
+                   den = 1;
+                   //считаем знаменатель разделенной разности
+                   for (k = 0; k <= i; k++)
+                   {
+                       if (k != j) den *= (xval[j] - xval[k]);
+                   }
+                   //считаем разделенную разность
+                   f += node[j] / den;
+               }
+               
+               //домножаем разделенную разность на скобки (x-x[0])...(x-x[i-1])
+               for (k = 0; k < i; k++) f *= (Math.Round(x,5) - xval[k]);
+               res += f;//полином
+           }
+           return res;
+       } 
+#region Drawing
+         public void DrawGraph()
+         {
+             int k = 0;
+             double tempsko = 0;
+             double x0 = 0.01936;
+             double step = 2;
+             int xmax = Convert.ToInt16(comboBox1.Text) + 1;
+             dataGridView1.RowCount = xmax - 1;
+             var nodes = new double[xmax];
+             var xval = new double[xmax];
+             const string outp1 = "SKO<<Emin=> Аппроксимация физически недостоверна!";
+             const string outp2 = "SKO>>Emin=> Аппроксимация физически недостоверна!";
+             const string equation = "  =>y=a+b/x\nSKO=";
+
+             GraphPane pane = zedGraph1.GraphPane;
+             pane.CurveList.Clear();
+             pane.Title.Text = "";
+           
+             var mlist = new PointPairList();
+             var LGList = new PointPairList();
+             var NTList = new PointPairList();
+             var MNKList = new PointPairList();
+  
+             step /= xmax;
+            
+             for (double i =x0; i <= 2; i+=step)
+             {
+                 nodes[k] = M(i);
+                 xval[k] = i;
+                 k++;
+             }
+             k = 0;
+             double[] ELN =new double[xmax-1];
+             double[] ENT = new double[xmax-1];
+             double[] EMNK = new double[xmax-1];
+             for (double x =x0; x <= 2; x +=step)
+             {                              
+                 LGList.Add(x, PnLagrange(x, nodes,xval)); 
+                 NTList.Add(x, Newton(xval,nodes,x));
+                 MNKList.Add(x, Apr(x, nodes,k,xval));
+                 tempsko+= SKO(x, nodes, k, xval);
+                 k++;
+             }
+
+             tempsko /= xmax;
+             for (double x = x0; x <= 2; x += 0.1)
+             {
+                 mlist.Add(x, M(x));
+             }
+
+             for (int x = 0; x < xmax - 1; x++)
+             {
+                 dataGridView1[0, x].Value = xval[x];
+                 dataGridView1[1, x].Value = mlist[x].Y;
+                 dataGridView1[2, x].Value = LGList[x].Y;
+                 dataGridView1[3, x].Value = NTList[x].Y;
+                 dataGridView1[4, x].Value = MNKList[x].Y;
+
+                 ELN[x] = Math.Abs(mlist[x].Y - LGList[x].Y);
+                 ENT[x] = Math.Abs(mlist[x].Y - NTList[x].Y);
+                 EMNK[x] = Math.Abs(mlist[x].Y - MNKList[x].Y);
+             }
+             double EmaxLN = ELN.Max();
+             double EmaxNT = ENT.Max();
+             double EmaxMNK = EMNK.Max();
+             lblE.Text = "Emax(LG)=" + EmaxLN.ToString("N3") + "\nEmax(NT)=" + EmaxNT.ToString("N3") + "\nEmax(MNK)=" + EmaxMNK.ToString("N3");
+
+             pane.AddCurve("f(x)", mlist, Color.Thistle, SymbolType.None);
+             pane.AddCurve("LaGrange(x)", LGList, Color.Red, SymbolType.Triangle);
+             pane.AddCurve("Newton(x)", NTList, Color.Blue, SymbolType.TriangleDown);
+             LineItem curvemy=  pane.AddCurve("MNK(x)", MNKList, Color.Indigo, SymbolType.Star);
+             curvemy.Line.IsVisible = false;
+
+             int outcount=0;
+             string outE = Ee(nodes, xval, xmax, ref outcount).ToString();
+             
+             pane.XAxis.Scale.Min = 0;
+             pane.XAxis.Scale.Max = 2;
+             pane.YAxis.Scale.Min = -5;
+             pane.YAxis.Scale.Max = 20;
+        
+
+             label2.Text = @"Emin(" + (outcount+1) + @")=" + outE + equation+tempsko;
+             if (Convert.ToDouble(tempsko)*100 < Convert.ToDouble(outE))
+                 label3.Text = outp1;
+             if (Convert.ToDouble(tempsko)  > Convert.ToDouble(outE))
+                 label3.Text = outp2;
+             zedGraph1.AxisChange();
+             zedGraph1.Invalidate();                       
+         }
+#endregion
+         private void button1_Click(object sender, EventArgs e)
+         {
+             dataGridView1.Rows.Clear();
+                 
+            zedGraph1.Visible = true;
+            DrawGraph();
+            
+         }
+
+         private void Form1_Load(object sender, EventArgs e)
+         {
+             comboBox1.Text = @"10";
+         }
+
+    }
+}
